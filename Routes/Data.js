d6c56bbf3bb312bc0fb48d92824e16975472dfcf
manvendra <manvendrachauhan535@gmail.com>
2022-10-27 02:29:30 +0530

    nodeproject

---
+++ Routes/Data.js
@@ -0,0 +1,8 @@
+const express =require('express');
+
+const server=require("../Controllers/Data")
+
+const dataRouter=express.Router();
+
+dataRouter.route("/home").get(server.apicontroller)
+module.exports=dataRouter
